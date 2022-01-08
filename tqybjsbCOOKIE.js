8314f13f56ea33276b899c6e5a967ad3ba06cd71
moneyfly1 <93109844+moneyfly1@users.noreply.github.com>
2022-01-08 14:49:15 +0800

    Update tqybjsbCOOKIE.js

--- tqybjsbCOOKIE.js
+++ tqybjsbCOOKIE.js
@@ -60,7 +60,7 @@ module.exports = {
     "favIconColor": "grey",
     "datas": [{
       "key": "tqyburl",
-      "val": ""
+      "val": "http://coin.makingmoney.cn:8088/task/coin?guid=UUID-DF9DFFE7-F94A-4F51-86C0-A194264093B5&ver=1.3.0&systemver=15.0.2&bid=com.swx.weather.app&mt=1&sw=414&sh=736&device=iPhone&idiom=0&chl=AppStore&language=zh-Hans-CN&locale=CN&zone=Asia%2FShanghai&timestamp=1641623078&idfv=EF8F615A-2946-4BBC-BE7B-B8068E485EA7&sign=663a31973440978811409c7190de7c15"
     }, {
       "key": "tqyburl2",
       "val": ""
